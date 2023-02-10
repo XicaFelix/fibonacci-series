79519b08b1f49ce9ccffdb45658472cf0980b807
Xica Felix <xicasfelix@gmail.com>
2023-02-10 11:23:14 -0500

    commit tree 291e227cc81979c9f923a7ea3ba7a4d79378e82c

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
   // type your code here
+  let gldnRatio = (1+ Math.sqrt(5))/2
+  if(num == 0){
+    return num;
+  }else{
+    let result = ((gldnRatio**num) - ((-gldnRatio)**-num))/ Math.sqrt(5)
+    return Math.floor(result);
+  }
+ 
 }
 
 if (require.main === module) {
@@ -22,3 +30,7 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// For a given number n, in a sequence of fibonacci numbers f(0) = 0 , f(1) = 1 f(2) = 1 f(3) = 2
+//  actual number in the sequence = n - 3
+// 
